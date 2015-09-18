efa97ca55dc1a6d618922c3c2ea3306a6ccf85a1
Clarke <Clarke@Clarkes-MacBook-Pro.local>
2015-09-17 18:57:24 -0700

    Homework 2 - updated gamma.py

--- gamma.py
+++ gamma.py
@@ -20,6 +20,7 @@ last_total = 0
 total_hold = 0
 dx = .1
 frac = ''
+steps = 0
 
 def h(x,t):
     return (x**(t-1))*(e**(-x))
@@ -28,6 +29,7 @@ def gamma(t):
     global total
     global last_total
     global total_hold
+    global steps
     if 100 >= t >= 1:
         if type(t) == int:
             return factorial(t-1)
@@ -35,12 +37,12 @@ def gamma(t):
             global frac
             frac = 'yes'
             total = 0
-            for x in numpy.arange (0, 1001):
+            for x in numpy.arange (0.0000000001, 1001):
                 total += (dx/6)*(h(x,t) +h(x+dx,t) +4*h((2*x+dx)/2,t))
+                steps+=1
                 if((dx/6)*(h(x,t) +h(x+dx,t) +4*h((2*x+dx)/2,t)) != 0):
-                	total_hold = (dx/6)*(h(x,t) +h(x+dx,t) +4*h((2*x+dx)/2,t))
-                if(x >= 1000):
-                	last_total = total_hold
+                #steps+=1
+		    last_total = total/steps
             return float(total)
     else:
         return 'Please enter a value between 1 and 100.'
@@ -54,7 +56,7 @@ def frac_diff():
 if __name__ =="__main__":
     t = (7./2)
     import doctest
-    doctest.testmod()    
+    #doctest.testmod()    
     print 'Gamma of',t,'is:',gamma(t)
     if frac == 'yes':
-        print 'frac_diff =', "{:0.7f}".format(frac_diff()*100),"%"
+        print 'frac_diff =', "{:0.2f}".format(frac_diff()*100),"%"
